fcb7683cf3203ca7e2ddaab39f1e1c0ac3d5ed0e
Dmitry Ivanov <d.k.ivanov@live.com>
2022-01-06 14:43:15 +0100

    CS 291: polygon location exercise

--- cs291_interactive_3d_rendering/unit_02/4_polygon-location.js
+++ cs291_interactive_3d_rendering/unit_02/4_polygon-location.js
@@ -1,34 +1,37 @@
 "use strict";
 
-// Polygon Creation Exercise
-// Your task is to complete the function PolygonGeometry(sides)
-// which takes 1 argument: sides
-// Return the mesh that defines the minimum number of triangles necessary to draw the polygon.
-// Radius of the polygon is 1. Center of the polygon is at 0, 0.
+// Polygon Location Exercise
+// Your task is to write a function that will take 2 arguments:
+//   sides - how many edges the polygon has.
+//   location - location of the center of the polygon as a THREE.Vector3.
+// Return the mesh that defines the minimum number of triangles necessary
+// to draw the polygon.
 
 /*global THREE, Coordinates, $, document*/
 
 var camera, scene, renderer;
 var windowScale;
 
-function PolygonGeometry(sides)
+function PolygonGeometry(sides, location)
 {
     var geometry = new THREE.BufferGeometry();
 
     // Generate Vertices and Indices
     const vertices = []
     const indices = []
-    for (var pt = 0; pt < sides; pt++)
+    for (let pt = 0; pt < sides; pt++)
     {
         // Add 90 degrees so we start at +Y axis, rotate counterclockwise around
         let angle = (Math.PI / 2) + (pt / sides) * 2 * Math.PI;
         let x = Math.cos(angle);
         let y = Math.sin(angle);
         // console.log("X:" + x + ",\t "+ "Y:" + y);
-        vertices.push(new THREE.Vector3(x, y, 0));
-        if(pt >=2)
+        // let vertex = new THREE.Vector3(x + location.x, y +location.y, 0 + location.z);
+        let vertex = new THREE.Vector3(x, y, 0).add(location);
+        vertices.push(vertex);
+        if (pt >= 2)
         {
-            indices.push(0, pt-1, pt)
+            indices.push(0, pt - 1, pt)
         }
     }
     // console.log(vertices);
@@ -42,13 +45,13 @@ function PolygonGeometry(sides)
 function init()
 {
     var canvasWidth = window.innerWidth;
-    var canvasHeight = window.innerHeight-50;
+    var canvasHeight = window.innerHeight - 50;
     var canvasRatio = canvasWidth / canvasHeight;
     // scene
     scene = new THREE.Scene();
 
     // Camera: Y up, X right, Z up
-    windowScale = 4;
+    windowScale = 8;
     var windowWidth = windowScale * canvasRatio;
     var windowHeight = windowScale;
 
@@ -58,7 +61,7 @@ function init()
         0, 40
     );
 
-    var focus = new THREE.Vector3(0, 1, 0);
+    var focus = new THREE.Vector3(3, 3, 0);
     camera.position.x = focus.x;
     camera.position.y = focus.y;
     camera.position.z = 10;
@@ -69,15 +72,15 @@ function init()
     renderer.gammaOutput = true;
     renderer.setSize(canvasWidth, canvasHeight);
     renderer.setClearColor(new THREE.Color(0xFFFFFF), 1.0);
-}
 
+}
 function showGrids()
 {
+    // Background grid and axes. Grid step size is 1, axes cross at 0, 0
     Coordinates.drawGrid({ size: 100, scale: 1, orientation: "z" });
-    Coordinates.drawAxes({ axisLength: 4, axisOrientation: "x", axisRadius: 0.02 });
-    Coordinates.drawAxes({ axisLength: 3, axisOrientation: "y", axisRadius: 0.02 });
+    Coordinates.drawAxes({ axisLength: 8, axisOrientation: "x", axisRadius: 0.02 });
+    Coordinates.drawAxes({ axisLength: 7, axisOrientation: "y", axisRadius: 0.02 });
 }
-
 function addToDOM()
 {
     var container = document.getElementById('container');
@@ -94,13 +97,12 @@ function render()
     renderer.render(scene, camera);
 }
 
+// Main body of the script
 try
 {
     init();
     showGrids();
-    var geo = PolygonGeometry(5);
-    // var geo = PolygonGeometry(6);
-    // var geo = PolygonGeometry(8);
+    var geo = PolygonGeometry(6, new THREE.Vector3(3, 4, 0));
     var material = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.FrontSide });
     var mesh = new THREE.Mesh(geo, material);
     scene.add(mesh);
