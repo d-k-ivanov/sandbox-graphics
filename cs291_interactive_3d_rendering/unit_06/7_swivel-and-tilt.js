af5ed7578de4187aaa4f99b820b6ddc3c5d32d12
Dmitry Ivanov <d.k.ivanov@live.com>
2022-02-09 02:26:45 +0100

    cs291_interactive_3d_rendering: 2022-09-02-02-26 Lights

---
+++ cs291_interactive_3d_rendering/unit_06/7_swivel-and-tilt.js
@@ -0,0 +1,295 @@
+"use strict";
+
+// Make a swivel and tilt light
+
+/*global THREE, Coordinates, document, dat, window, $*/
+
+var camera, scene, renderer;
+var cameraControls, effectController;
+var clock = new THREE.Clock();
+var light;
+
+function fillScene()
+{
+    scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xAAAAAA, 2000, 4000);
+
+    // LIGHTS
+    // scene.add(new THREE.AmbientLight(0x222222));
+
+    light = new THREE.DirectionalLight(0xFFFFFF, 1.5);
+    light.position.set(1, 1, 0);
+    scene.add(light);
+
+    // MATERIALS
+    var headMaterial = new THREE.MeshLambertMaterial();
+    headMaterial.color.r = 104 / 255;
+    headMaterial.color.g = 1 / 255;
+    headMaterial.color.b = 5 / 255;
+
+    var hatMaterial = new THREE.MeshPhongMaterial({ shininess: 100 });
+    hatMaterial.color.r = 24 / 255;
+    hatMaterial.color.g = 38 / 255;
+    hatMaterial.color.b = 77 / 255;
+    hatMaterial.specular.setRGB(0.5, 0.5, 0.5);
+
+    var bodyMaterial = new THREE.MeshPhongMaterial({ shininess: 100 });
+    bodyMaterial.color.setRGB(31 / 255, 86 / 255, 169 / 255);
+    bodyMaterial.specular.setRGB(0.5, 0.5, 0.5);
+
+    var glassMaterial = new THREE.MeshPhongMaterial({ color: 0x0, specular: 0xFFFFFF, shininess: 100, opacity: 0.3, transparent: true });
+
+    var legMaterial = new THREE.MeshPhongMaterial({ shininess: 4 });
+    legMaterial.color.setHex(0xAdA79b);
+    legMaterial.specular.setRGB(0.5, 0.5, 0.5);
+
+    var footMaterial = new THREE.MeshPhongMaterial({ color: 0x960f0b, shininess: 30 });
+    footMaterial.specular.setRGB(0.5, 0.5, 0.5);
+
+    var crossbarMaterial = new THREE.MeshPhongMaterial({ color: 0x808080, specular: 0xFFFFFF, shininess: 400 });
+    var eyeMaterial = new THREE.MeshPhongMaterial({ color: 0x000000, specular: 0x303030, shininess: 4 });
+
+    var sphere, cylinder, cube;
+
+    var bevelRadius = 1.9;	// TODO: 2.0 causes some geometry bug.
+
+    // MODELS
+    // base
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(20 + 64 + 110, 4, 2 * 77 + 12, bevelRadius), footMaterial);
+    cube.position.x = -45;
+    cube.position.y = 4 / 2;
+    cube.position.z = 0;
+    scene.add(cube);
+
+    // feet
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(20 + 64 + 110, 52, 6, bevelRadius), footMaterial);
+    cube.position.x = -45;
+    cube.position.y = 52 / 2;
+    cube.position.z = 77 + 6 / 2;
+    scene.add(cube);
+
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(20 + 64 + 110, 52, 6, bevelRadius), footMaterial);
+    cube.position.x = -45;
+    cube.position.y = 52 / 2;
+    cube.position.z = -(77 + 6 / 2);
+    scene.add(cube);
+
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(64, 104, 6, bevelRadius), footMaterial);
+    cube.position.x = 0;
+    cube.position.y = 104 / 2;
+    cube.position.z = 77 + 6 / 2;
+    scene.add(cube);
+
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(64, 104, 6, bevelRadius), footMaterial);
+    cube.position.x = 0;
+    cube.position.y = 104 / 2;
+    cube.position.z = -(77 + 6 / 2);
+    scene.add(cube);
+
+    // legs
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(60, 282 + 4, 4, bevelRadius), legMaterial);
+    cube.position.x = 0;
+    cube.position.y = 104 + 282 / 2 - 2;
+    cube.position.z = 77 + 6 / 2;
+    scene.add(cube);
+
+    cube = new THREE.Mesh(
+        new THREE.RoundedBoxGeometry(60, 282 + 4, 4, bevelRadius), legMaterial);
+    cube.position.x = 0;
+    cube.position.y = 104 + 282 / 2 - 2;
+    cube.position.z = -(77 + 6 / 2);
+    scene.add(cube);
+
+    // body
+    sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(104 / 2, 32, 16, 0, Math.PI * 2, Math.PI / 2, Math.PI), bodyMaterial);
+    sphere.position.x = 0;
+    sphere.position.y = 160;
+    sphere.position.z = 0;
+    scene.add(sphere);
+
+    // cap for top of hemisphere
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(104 / 2, 104 / 2, 0, 32), bodyMaterial);
+    cylinder.position.x = 0;
+    cylinder.position.y = 160;
+    cylinder.position.z = 0;
+    scene.add(cylinder);
+
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(12 / 2, 12 / 2, 390 - 100, 32), bodyMaterial);
+    cylinder.position.x = 0;
+    cylinder.position.y = 160 + 390 / 2 - 100;
+    cylinder.position.z = 0;
+    scene.add(cylinder);
+
+    // glass stem
+    sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(116 / 2, 32, 16), glassMaterial);
+    sphere.position.x = 0;
+    sphere.position.y = 160;
+    sphere.position.z = 0;
+    scene.add(sphere);
+
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(24 / 2, 24 / 2, 390, 32), glassMaterial);
+    cylinder.position.x = 0;
+    cylinder.position.y = 160 + 390 / 2;
+    cylinder.position.z = 0;
+    scene.add(cylinder);
+
+    // head
+    sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(104 / 2, 32, 16), headMaterial);
+    sphere.position.x = 0;
+    sphere.position.y = 160 + 390;
+    sphere.position.z = 0;
+    scene.add(sphere);
+
+    // hat
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(142 / 2, 142 / 2, 10, 32), hatMaterial);
+    cylinder.position.x = 0;
+    cylinder.position.y = 160 + 390 + 40 + 10 / 2;
+    cylinder.position.z = 0;
+    scene.add(cylinder);
+
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(80 / 2, 80 / 2, 70, 32), hatMaterial);
+    cylinder.position.x = 0;
+    cylinder.position.y = 160 + 390 + 40 + 10 + 70 / 2;
+    cylinder.position.z = 0;
+    scene.add(cylinder);
+
+    // crossbar
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(5, 5, 200, 32), crossbarMaterial);
+    cylinder.position.set(0, 360, 0);
+    cylinder.rotation.x = 90 * Math.PI / 180.0;
+    scene.add(cylinder);
+
+    // nose
+    cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(6, 14, 70, 32), headMaterial);
+    cylinder.position.set(-70, 530, 0);
+    cylinder.rotation.z = 90 * Math.PI / 180.0;
+    scene.add(cylinder);
+
+    // eyes
+    var sphGeom = new THREE.SphereGeometry(10, 32, 16);
+
+    // left eye
+    sphere = new THREE.Mesh(sphGeom, eyeMaterial);
+    sphere.position.set(-48, 560, 0);
+    var eye = new THREE.Object3D();
+    eye.add(sphere);
+    eye.rotation.y = 20 * Math.PI / 180.0;
+    scene.add(eye);
+
+    // right eye
+    sphere = new THREE.Mesh(sphGeom, eyeMaterial);
+    sphere.position.set(-48, 560, 0);
+    eye = new THREE.Object3D();
+    eye.add(sphere);
+    eye.rotation.y = -20 * Math.PI / 180.0;
+    scene.add(eye);
+}
+
+function init()
+{
+    var canvasWidth = window.innerWidth;
+    var canvasHeight = window.innerHeight - 50;
+    var canvasRatio = canvasWidth / canvasHeight;
+
+    // RENDERER
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(canvasWidth, canvasHeight);
+    renderer.setClearColor(new THREE.Color(0xAAAAAA), 1.0);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    // CAMERA
+    camera = new THREE.PerspectiveCamera(35, canvasRatio, 1, 4000);
+    camera.position.set(-1160, 350, -600);
+
+    // CONTROLS
+    cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+    cameraControls.target.set(0, 310, 0);
+}
+
+function drawHelpers()
+{
+    Coordinates.drawGround({ size: 10000 });
+    Coordinates.drawGrid({ size: 10000, scale: 0.01 });
+}
+
+function addToDOM()
+{
+    var container = document.getElementById('container');
+    var canvas = container.getElementsByTagName('canvas');
+    if (canvas.length > 0)
+    {
+        container.removeChild(canvas[0]);
+    }
+    container.appendChild(renderer.domElement);
+}
+
+function animate()
+{
+    window.requestAnimationFrame(animate);
+    render();
+}
+
+function render()
+{
+    var delta = clock.getDelta();
+    cameraControls.update(delta);
+
+    // Light control - Altitude
+    let xAlt = Math.cos(effectController.altitude * Math.PI / 180);
+    let y    = Math.sin(effectController.altitude * Math.PI / 180);
+
+    // Light control - Azimuth
+    let xAz = Math.cos(effectController.azimuth * Math.PI / 180);
+    let zAz   = Math.sin(effectController.azimuth * Math.PI / 180);
+
+    var length = Math.sqrt(1 - y * y);
+    let x = length * xAz;
+    let z = length * zAz;
+
+    light.position.x = x;
+    light.position.y = y;
+    light.position.z = z;
+
+    renderer.render(scene, camera);
+}
+
+function setupGui()
+{
+    // Horizontal coordinate system
+    effectController = {
+        azimuth: 160,
+        altitude: 60
+    };
+    var gui = new dat.GUI();
+    gui.add(effectController, "azimuth", 0.0, 360.0).name("azimuth");
+    gui.add(effectController, "altitude", -90.0, 90.0).name("altitude");
+}
+
+try
+{
+    init();
+    fillScene();
+    setupGui();
+    drawHelpers();
+    addToDOM();
+    animate();
+} catch (e)
+{
+    alert(e);
+}
