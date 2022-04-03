8360677ad0fbe254795f28c83a79ed8691d86963
Dmitry Ivanov <d.k.ivanov@live.com>
2022-04-03 22:49:08 +0200

    cs291_interactive_3d_rendering: 2022-03-04-22-49

---
+++ opengl-playgound-01/run_web_server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# python >= 3.4
+
+import http.server
+import socketserver
+
+class SimpleHTTPRequestHandlerNoCache(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.no_cache_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def no_cache_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+
+if __name__ == '__main__':
+    PORT = 8080
+    # Handler = http.server.SimpleHTTPRequestHandler # Default
+    Handler = SimpleHTTPRequestHandlerNoCache
+
+    Handler.extensions_map={
+        '.css': 'text/css',
+        '.html': 'text/html',
+        '.jpg': 'image/jpg',
+        '.js': 'application/x-javascript',
+        '.manifest': 'text/cache-manifest',
+        '.png': 'image/png',
+        '.svg': 'image/svg+xml',
+        '.webapp': 'application/x-web-app-manifest+json',
+        '': 'application/octet-stream', # Default
+    }
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+
+    print("Server started at http://localhost:" + str(PORT))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Stopping http server...")
+        httpd.shutdown()
+        httpd.server_close()
