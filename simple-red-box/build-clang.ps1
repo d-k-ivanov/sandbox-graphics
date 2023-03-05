799d7d696f94879806240635aa3d4f6d17cc49f6
Dmitry Ivanov <d.k.ivanov@live.com>
2023-03-06 00:18:03 +0100

    move simple bot from cpp

---
+++ simple-red-box/build-clang.ps1
@@ -0,0 +1,2 @@
+#!/usr/bin/env pwsh
+clang --std=c++17 -O3 -DNDEBUG -o bin/simple-red-box.exe simple-red-box.cpp
---
+++ simple-red-box/build-mingw.ps1
@@ -0,0 +1,2 @@
+#!/usr/bin/env pwsh
+g++ --std=c++17 -O3 -DNDEBUG -o bin/simple-red-box.exe simple-red-box.cpp
