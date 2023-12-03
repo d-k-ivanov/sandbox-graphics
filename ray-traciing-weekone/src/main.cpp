c79418fcdfc350f4bc43f9bf6537bb1970b68bfe
Dmitry Ivanov <d.k.ivanov@live.com>
2023-12-03 23:13:56 +0100

    Creating scene rays

--- ray-traciing-weekone/src/main.cpp
+++ ray-traciing-weekone/src/main.cpp
@@ -1,17 +1,52 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+color ray_color(const ray &r)
+{
+    return {0, 0, 0};
+}
+
 int main()
 {
+    // Aspect ratios:
+    // width / height =  2 / 1 = 2
+    // width / height = 16 / 9 = 1.7778
     // Image
-    constexpr int image_width = 256;
-    constexpr int image_height = 256;
+    // constexpr int image_width = 256;
+    // constexpr int image_height = 256;
+
+    constexpr auto aspect_ratio = 16.0 / 9.0;
+    constexpr int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    constexpr auto focal_length = 1.0;
+    constexpr auto viewport_height = 2.0;
+    const auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+    const auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    const auto viewport_u = vec3(viewport_width, 0, 0);
+    const auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    const auto pixel_delta_u = viewport_u / image_width;
+    const auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    const auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    const auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
-    std::cout << "P3\n"
-              << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n";
+    std::cout << image_width << ' ' << image_height << "\n";
+    std::cout << "255\n";
 
     for(int j = 0; j < image_height; ++j)
     {
@@ -19,10 +54,15 @@ int main()
         for(int i = 0; i < image_width; ++i)
         {
             // Generate colours:
-            const auto r = static_cast<double>(i) / (image_width - 1);
-            const auto g = static_cast<double>(j) / (image_height - 1);
-            const auto b = 0;
-            const auto pixel_color = color(r, g, b);
+            // const auto r = static_cast<double>(i) / (image_width - 1);
+            // const auto g = static_cast<double>(j) / (image_height - 1);
+            // const auto b = 0;
+            // const auto pixel_color = color(r, g, b);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            const color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
