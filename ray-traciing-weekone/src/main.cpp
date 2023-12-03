f6605461765573c2cbe8564060f035d400fd9324
Dmitry Ivanov <d.k.ivanov@live.com>
2023-12-03 20:16:18 +0100

    add vector 3 class

--- ray-traciing-weekone/src/main.cpp
+++ ray-traciing-weekone/src/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     // Image
-    int image_width = 256;
-    int image_height = 256;
+    constexpr int image_width  = 256;
+    constexpr int image_height = 256;
 
     // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; ++j) {
+    for(int j = 0; j < image_height; ++j)
+    {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+        for(int i = 0; i < image_width; ++i)
+        {
+            auto r = static_cast<double>(i) / (image_width - 1);
+            auto g = static_cast<double>(j) / (image_height - 1);
             auto b = 0;
 
             int ir = static_cast<int>(255.999 * r);
