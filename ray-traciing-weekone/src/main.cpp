a0d0dd185367ecc97f64a32a6623efae4d2ec438
Dmitry Ivanov <d.k.ivanov@live.com>
2023-12-03 19:05:45 +0100

    Adding a Progress Indicator

--- ray-traciing-weekone/src/main.cpp
+++ ray-traciing-weekone/src/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
 int main() {
-
     // Image
-
     int image_width = 256;
     int image_height = 256;
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -24,4 +22,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::clog << "\rDone.                 \n";
 }
