3c736462cea604dda69e73b26f772d36ef7f641a
Dmitry Ivanov <d.k.ivanov@live.com>
2023-12-04 11:42:08 +0100

    ray-tracing: drawing normals

--- ray-traciing-weekone/src/main.cpp
+++ ray-traciing-weekone/src/main.cpp
@@ -4,26 +4,32 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const ray &r)
+double hit_sphere(const point3 &center, double radius, const ray &r)
 {
     const vec3 oc = r.origin() - center;
     const auto a = dot(r.direction(), r.direction());
     const auto b = 2.0 * dot(oc, r.direction());
     const auto c = dot(oc, oc) - radius * radius;
     const auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+    if(discriminant < 0)
+    {
+        return -1.0;
+    }
+    return (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 color ray_color(const ray &r, int pixel_w)
 {
-    if(hit_sphere(point3(0, 0, -1), 0.5, r))
+    const auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if(t > 0.0)
     {
-        return {1, 0, 0};
+        const vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     // blendedValue = (1 − a) * startValue + a * endValue
     const vec3 unit_direction = unit_vector(r.direction());
     const auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + (pixel_w % 2 == 0 ? a * color(0.5, 0.7, 1.0): a * color(0.5, 1.0, 0.7));
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + (pixel_w % 2 == 0 ? a * color(0.5, 0.7, 1.0) : a * color(0.5, 1.0, 0.7));
 }
 
 int main()
